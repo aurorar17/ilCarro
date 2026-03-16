27b3c95c355977c33de40bf76335b443167e0613
aurorar17 <aurora.roilo01@gmail.com>
2026-03-16 16:53:38 -0700

    deploy

--- ilCarroApp/src/pages/Menu.jsx
+++ ilCarroApp/src/pages/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import "../styles/Menu.css";
+import "../styles/menu.css";
 
 const Menu = () => {
   const [showScrollButton, setShowScrollButton] = React.useState(false);
--- ilCarroApp/src/pages/MenuDE.jsx
+++ ilCarroApp/src/pages/MenuDE.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import "../styles/Menu.css";
+import "../styles/menu.css";
 
 const MenuDE = () => {
   const [showScrollButton, setShowScrollButton] = React.useState(false);
