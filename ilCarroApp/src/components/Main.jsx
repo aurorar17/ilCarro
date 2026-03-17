f2cec1ae03d6360018cd63bddda042403c983e17
aurorar17 <aurora.roilo01@gmail.com>
2026-03-16 17:01:31 -0700

    deploy 3

--- ilCarroApp/src/components/Main.jsx
+++ ilCarroApp/src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Gallery from "./Gallery";
 import "../styles/main-section.css";
-import TripAdv from "./Tripadv";
+import TripAdv from "./TripAdv";
 
 const Main = () => {
   return (
